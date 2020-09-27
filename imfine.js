a7a4bb8a69efb00d877c9963e1451295bcfc91d5
KamiKazeTeddy <50114234+KamiKazeTeddy@users.noreply.github.com>
2020-09-28 01:41:23 +0200

    Add files via upload
    Remove some old code

--- imfine.js
+++ imfine.js
@@ -43,30 +43,3 @@ function setGYCookie(tab) {
         }
     }
 }
-
-/*
-
-chrome.tabs.onActivated.addListener(function(tabInfo) {
-    chrome.tabs.get(tabInfo.tabId, function(tab) {
-        console.log(tab.url); //Outputs the current URL (DEBUG)
-        
-        if ((tab.url).search("google.com") != -1 || (tab.url).search("youtube.com") != -1 || (tab.url).search("youtu.be") != -1)
-        {
-            console.log("if1") //DEBUG
-            chrome.cookies.set({url: "https://www.google.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".google.com"}) //Google Cookies
-            console.log("Google found. Cookie set.")
-
-            if((tab.url).search("youtube.com") != -1 || (tab.url).search("youtu.be") != -1)
-            {
-                console.log("if2") //DEBUG
-                chrome.cookies.set({url: "https://www.youtube.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".youtube.com"}) //Youtube Cookie
-                chrome.cookies.set({url: "https://www.youtube.com", name: "NID", value: "204=wvaNJqiHUGet-fvFDfVVdE1ZSB8-2mW1zd4jqi5voEl-E1ZT5XYPU7ag08kOPuuPLsGlBojjjBSHnSs3RIimsB0wNj9a_m1S7sd6WSVZvPxcwJb_9j-HLM-1aLbtQn7sOBw1TIohf4Ncvj3RMRDQtTU6MVGs8_XU641TjDp9qY0", domain: ".youtube.com"})
-                console.log("Youtube found. Cookie set.")
-            }
-        }
-    });
-});
-
-
-
-*/
