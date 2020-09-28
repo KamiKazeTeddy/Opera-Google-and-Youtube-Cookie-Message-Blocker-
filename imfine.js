260b2af91b3696dc27243f4eca572ee6e28ed728
KamiKazeTeddy <50114234+KamiKazeTeddy@users.noreply.github.com>
2020-09-28 02:08:22 +0200

    Add files via upload
    Fixes. Remove useless code

--- imfine.js
+++ imfine.js
@@ -2,6 +2,31 @@ chrome.cookies.set({url: "https://www.google.com", name: "CONSENT", value: "YES+
 chrome.cookies.set({url: "https://www.youtube.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".youtube.com"}) //Youtube Cookie
 chrome.cookies.set({url: "https://www.youtube.com", name: "NID", value: "204=wvaNJqiHUGet-fvFDfVVdE1ZSB8-2mW1zd4jqi5voEl-E1ZT5XYPU7ag08kOPuuPLsGlBojjjBSHnSs3RIimsB0wNj9a_m1S7sd6WSVZvPxcwJb_9j-HLM-1aLbtQn7sOBw1TIohf4Ncvj3RMRDQtTU6MVGs8_XU641TjDp9qY0", domain: ".youtube.com"})
 
+function setGYCookie(tab) {
+    if(tab.pendingUrl !== undefined)
+    {
+        if ((tab.pendingUrl).search("google.com") != -1 || (tab.pendingUrl).search("youtube.com") != -1 || (tab.pendingUrl).search("youtu.be") != -1 || (tab.pendingUrl).search("chrome://startpage/private") != -1)
+        {
+            chrome.cookies.set({url: "https://www.google.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".google.com"}) //Google Cookies
+            chrome.cookies.set({url: "https://www.youtube.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".youtube.com"}) //Youtube Cookie
+            chrome.cookies.set({url: "https://www.youtube.com", name: "NID", value: "204=wvaNJqiHUGet-fvFDfVVdE1ZSB8-2mW1zd4jqi5voEl-E1ZT5XYPU7ag08kOPuuPLsGlBojjjBSHnSs3RIimsB0wNj9a_m1S7sd6WSVZvPxcwJb_9j-HLM-1aLbtQn7sOBw1TIohf4Ncvj3RMRDQtTU6MVGs8_XU641TjDp9qY0", domain: ".youtube.com"})
+            console.log("Google + Youtube Cookie set.")
+        }
+    }
+    
+    
+    if(tab.url !== undefined)
+    {
+        if ((tab.url).search("google.com") != -1 || (tab.url).search("youtube.com") != -1 || (tab.url).search("youtu.be") != -1 || (tab.url).search("chrome://startpage/private") != -1)
+        {
+            chrome.cookies.set({url: "https://www.google.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".google.com"}) //Google Cookies
+            chrome.cookies.set({url: "https://www.youtube.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".youtube.com"}) //Youtube Cookie
+            chrome.cookies.set({url: "https://www.youtube.com", name: "NID", value: "204=wvaNJqiHUGet-fvFDfVVdE1ZSB8-2mW1zd4jqi5voEl-E1ZT5XYPU7ag08kOPuuPLsGlBojjjBSHnSs3RIimsB0wNj9a_m1S7sd6WSVZvPxcwJb_9j-HLM-1aLbtQn7sOBw1TIohf4Ncvj3RMRDQtTU6MVGs8_XU641TjDp9qY0", domain: ".youtube.com"})
+            console.log("Google + Youtube Cookie set.")
+        }
+    }
+}
+
 chrome.tabs.onCreated.addListener(function(tabI) {
     console.log("1. PendingURL:" + tabI.pendingUrl);
     console.log("1. URL: " + tabI.url); //Outputs the current URL (DEBUG)
@@ -18,28 +43,3 @@ chrome.tabs.onCreated.addListener(function(tabI) {
     
     
 });
-
-function setGYCookie(tab) {
-    if(!(tab.pendingUrl === void(0)))
-    {
-        if ((tab.pendingUrl).search("google.com") != -1 || (tab.pendingUrl).search("youtube.com") != -1 || (tab.pendingUrl).search("youtu.be") != -1 || (tab.pendingUrl).search("chrome://startpage/private") != -1)
-        {
-            chrome.cookies.set({url: "https://www.google.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".google.com"}) //Google Cookies
-            chrome.cookies.set({url: "https://www.youtube.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".youtube.com"}) //Youtube Cookie
-            chrome.cookies.set({url: "https://www.youtube.com", name: "NID", value: "204=wvaNJqiHUGet-fvFDfVVdE1ZSB8-2mW1zd4jqi5voEl-E1ZT5XYPU7ag08kOPuuPLsGlBojjjBSHnSs3RIimsB0wNj9a_m1S7sd6WSVZvPxcwJb_9j-HLM-1aLbtQn7sOBw1TIohf4Ncvj3RMRDQtTU6MVGs8_XU641TjDp9qY0", domain: ".youtube.com"})
-            console.log("Google + Youtube Cookie set.")
-        }
-    }
-    
-    
-    if(!(tab.url === void(0)))
-    {
-        if ((tab.url).search("google.com") != -1 || (tab.url).search("youtube.com") != -1 || (tab.url).search("youtu.be") != -1 || (tab.pendingUrl).search("chrome://startpage/private") != -1)
-        {
-            chrome.cookies.set({url: "https://www.google.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".google.com"}) //Google Cookies
-            chrome.cookies.set({url: "https://www.youtube.com", name: "CONSENT", value: "YES+DE.de+V14+BX", domain: ".youtube.com"}) //Youtube Cookie
-            chrome.cookies.set({url: "https://www.youtube.com", name: "NID", value: "204=wvaNJqiHUGet-fvFDfVVdE1ZSB8-2mW1zd4jqi5voEl-E1ZT5XYPU7ag08kOPuuPLsGlBojjjBSHnSs3RIimsB0wNj9a_m1S7sd6WSVZvPxcwJb_9j-HLM-1aLbtQn7sOBw1TIohf4Ncvj3RMRDQtTU6MVGs8_XU641TjDp9qY0", domain: ".youtube.com"})
-            console.log("Google + Youtube Cookie set.")
-        }
-    }
-}
